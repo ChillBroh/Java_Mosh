df943bc59f83d3d08ad257b2d9b24795a77a85ad
ChillBroh <isharamadusanka410@gmail.com>
2022-07-18 03:01:40 +0530

    string reference type learned

---
+++ src/String.java
@@ -0,0 +1,9 @@
+public class String {
+    public static void main(String[] args) {
+        java.lang.String message = "hello world";
+        java.lang.String newmessage = message;
+        message = "yoyo";
+        System.out.println(newmessage);
+
+    }
+}
