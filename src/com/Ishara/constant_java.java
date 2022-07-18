c218787e088f0ec0623a8712c0719ebbb378a303
ChillBroh <isharamadusanka410@gmail.com>
2022-07-18 16:49:16 +0530

    created constant class to learn about const in java

---
+++ src/com/Ishara/constant_java.java
@@ -0,0 +1,10 @@
+package com.Ishara;
+
+public class constant_java {
+    public static void main(String[] args) {
+        final float PI = 3.14F; //use final and compiler treat this as const
+        System.out.println(PI);
+        //PI = 1; this will make an error
+
+    }
+}
