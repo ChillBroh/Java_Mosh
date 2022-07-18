602474cff0ec9b8397ffb45b9ed81f05ead4cbc1
ChillBroh <isharamadusanka410@gmail.com>
2022-07-18 14:54:23 +0530

    change class name from String to String_ex. cause ide occured error while running. There is String inbuilt class in Ide

--- src/com/Ishara/String_ex.java
+++ src/com/Ishara/String_ex.java
@@ -2,8 +2,10 @@ package com.Ishara;
 
 public class String_ex {
     public static void main(String[] args) {
-        java.lang.String message = "hello world";
+        java.lang.String message = new String("helloworld");
         java.lang.String newmessage = message;
+
+        System.out.println(newmessage);
         message = "yoyo";
         System.out.println(newmessage);
         System.out.println(message);
