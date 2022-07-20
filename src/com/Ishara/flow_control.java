6b89e7a2704c2c005a28933b2294a290c33adcca
ChillBroh <isharamadusanka410@gmail.com>
2022-07-20 15:21:12 +0530

    updated with switch

--- src/com/Ishara/flow_control.java
+++ src/com/Ishara/flow_control.java
@@ -4,5 +4,16 @@ public class flow_control {
     public static void main(String[] args){
         int income = 120_000;
         String className = income > 100_000 ? "First" : "Economy"; //using ternary operator
+
+        switch(className){
+            case "First" :
+                System.out.println("Congratulations You are in first class");
+                break;
+            case "Economy" :
+                System.out.println("Congratulations You are in economy class");
+                break;
+            default :
+                System.out.println("You are not eligible");
+        }
     }
 }
