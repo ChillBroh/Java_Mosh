214dcece96bfc89a6f451d5e4f7be45cdf353769
ChillBroh <isharamadusanka410@gmail.com>
2022-07-20 14:22:47 +0530

    used ternary operator

---
+++ src/com/Ishara/flow_control.java
@@ -0,0 +1,8 @@
+package com.Ishara;
+
+public class flow_control {
+    public static void main(String[] args){
+        int income = 120_000;
+        String className = income > 100_000 ? "First" : "Economy"; //using ternary operator
+    }
+}
